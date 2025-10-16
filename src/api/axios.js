8667eef88195cc76c14dafc1ddff376a3ec10814
rushikeshtaur12 <rushikeshtaur12@gmail.com>
2025-10-16 20:38:11 +0530

    code for chnage

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:"https://vasundhara-backend-production.up.railway.app/api" //use for localhost ("http://localhost:5000/api", )// your backend URL
-});
+  baseURL:"http://localhost:5000/api"// your backend URL //use for localhost 
+  // "https://vasundhara-backend-production.up.railway.app/api"    // for production
+}); 
